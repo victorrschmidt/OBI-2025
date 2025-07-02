d1fdf101a87761f8619331236facfd16ab5275d8
Victor Schmidt <101749261+victorrschmidt@users.noreply.github.com>
2025-07-01 21:03:38 -0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -4,18 +4,6 @@ using ll = long long;
 const char nl = '\n';
 const int INF = (int) 1e9;
 
-template<typename A, typename B> ostream& operator<<(ostream &os, const pair<A, B> &p) { return os << '(' << p.first << ", " << p.second << ')'; }
-template<typename... Args> ostream& operator<<(ostream& os, const tuple<Args...>& t) { os << '('; apply([&os](const Args&... args) { size_t n = 0; ((os << args << (++n != sizeof...(Args) ? ", " : "")), ...); }, t); return os << ')'; }
-template<typename T_container, typename T = typename enable_if<!is_same<T_container, string>::value, typename T_container::value_type>::type> ostream& operator<<(ostream &os, const T_container &v) { os << '{'; string sep; for (const T &x : v) os << sep << x, sep = ", "; return os << '}'; }
-void dbg_out() { cerr << endl; }
-template<typename Head, typename... Tail> void dbg_out(Head H, Tail... T) { cerr << ' ' << H; dbg_out(T...); }
-
-#ifdef LOCAL
-#define debug(...) cerr << "[DEBUG] (" << #__VA_ARGS__ << "):", dbg_out(__VA_ARGS__)
-#else
-#define debug(...)
-#endif
-
 int main() {
     ios::sync_with_stdio(false);
 #ifndef LOCAL
