05fddb90db37cebb0bf34e59afeaebbf51ff6b26
Victor Schmidt <101749261+victorrschmidt@users.noreply.github.com>
2025-08-19 13:11:33 -0300

    Update main.cpp

---
+++ fases/estadual/um_desafio_muito_distinto/main.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+using namespace std;
+using ll = long long;
+
+int main() {
+    int p;
+    cin >> p;
+
+    while (p--) {
+        ll l, a, b;
+        cin >> l >> a >> b;
+
+        ll esquerda = 0;
+        ll direita = b - a + 1;
+        ll melhor = -1;
+
+        while (esquerda <= direita) {
+            ll meio = (esquerda + direita) / 2;
+            ll soma = (a + a + meio - 1) * meio / 2;
+
+            if (soma >= l) {
+                direita = meio - 1;
+                melhor = meio;
+            }
+            else {
+                esquerda = meio + 1;
+            }
+        }
+
+        if (melhor != -1) {
+            cout << melhor << '\n';
+        }
+        else {
+            cout << b - a + 1 << '\n';
+        }
+    }
+
+    return 0;
+}
